58994745dd4867c7258300cf49b27fbc64d44a2f
JohnnyIsNotAvailable <kabevzhanbek@gmail.com>
2023-12-26 22:52:10 +0600

    comment

--- src/Users/TechSupportSpecialist.java
+++ src/Users/TechSupportSpecialist.java
@@ -1,7 +1,7 @@
 package Users;
 
 /**
- * This class represents a technical support specialist within the university who is an employee responsible for providing technical support to students, teachers, and staff.
+ * This class represents a technical support specialist within the university, who is an employee responsible for providing technical support to students, teachers, and staff.
  */
 public class TechSupportSpecialist extends Employee{
     private static final long serialVersionUID = 1L;
