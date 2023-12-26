87248a673376d6041ef392a9f15b1bccee0c3b9e
JohnnyIsNotAvailable <kabevzhanbek@gmail.com>
2023-12-26 22:50:53 +0600

    comment

--- src/Users/Teacher.java
+++ src/Users/Teacher.java
@@ -20,17 +20,51 @@ import Message.Message;
 import Utils.CustomExceptions.CourseNotFound;
 import Utils.CustomExceptions.UserNotFound;
 
+/**
+ * Represents a teacher in the system, extending the Employee class and implementing the Researcher interface.
+ *
+ * <p>Teachers have a teacher title, assigned courses with enrolled students, a rating, and can perform actions
+ * related to teaching and research.
+ */
 public class Teacher extends Employee implements Researcher, Serializable {
+
+	/**
+	 * Serial version UID for serialization.
+	 */
 	private static final long serialVersionUID = 1L;
+
+	/**
+	 * The teacher's title (e.g., Assistant Professor, Professor).
+	 */
 	private TeacherType teacherTitle;
+
+	/**
+	 * A map of courses taught by the teacher, with lists of students enrolled in each course.
+	 */
 	private HashMap<Course, List<Student>> courseWithStudents;
 
+	/**
+	 * The teacher's rating (e.g., based on student feedback).
+	 */
 	private double rate;
 
+	/**
+	 * Default constructor.
+	 */
 	public Teacher(){
 		super();
 	}
 
+	/**
+	 * Creates a new Teacher object with the specified details.
+	 *
+	 * @param userType the user type (always UserType.TEACHER)
+	 * @param id the teacher's unique ID
+	 * @param password the teacher's password
+	 * @param username the teacher's username
+	 * @param salary the teacher's salary
+	 * @param teacherTitle the teacher's title
+	 */
 	public Teacher(UserType userType, String id, String password, String username, double salary, TeacherType teacherTitle) {
 		super(userType, id, password, username, salary);
 		this.teacherTitle = teacherTitle;
@@ -39,6 +73,9 @@ public class Teacher extends Employee implements Researcher, Serializable {
 
 	}
 
+	/**
+	 * Prints a list of students enrolled in each course taught by the teacher.
+	 */
 	public void viewStudents() {
 		for(Map.Entry<Course, List<Student>> entry : courseWithStudents.entrySet()){
 			System.out.println("students for " + entry.getKey());
@@ -48,6 +85,11 @@ public class Teacher extends Employee implements Researcher, Serializable {
 		}
 	}
 
+	/**
+	 * Assigns courses and students to the teacher.
+	 *
+	 * @param courseWithStudents a map of courses and their associated students
+	 */
 	public void setCourseWithStudents(HashMap<Course, List<Student>> courseWithStudents) {
 		this.courseWithStudents = courseWithStudents;
 		for(Map.Entry<Course, List<Student>> entry : courseWithStudents.entrySet()){
@@ -57,6 +99,13 @@ public class Teacher extends Employee implements Researcher, Serializable {
 		}
 	}
 
+	/**
+	 * Allows the teacher to view a student's journal for a specific course.
+	 *
+	 * @return a Pair object containing the student and course
+	 * @throws IOException if an error occurs while reading input
+	 * @throws UserNotFound if the specified student or course is not found
+	 */
 	public Pair viewJournal() throws IOException, UserNotFound {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter course name: ");
@@ -81,6 +130,13 @@ public class Teacher extends Employee implements Researcher, Serializable {
 		return new Pair(s, c);
 	}
 
+	/**
+	 * Allows the teacher to add a point to a student's journal for a specific course.
+	 *
+	 * @throws IOException if an error occurs while reading input
+	 * @throws CourseNotFound if the specified course is not found
+	 * @throws UserNotFound if the specified student is not found
+	 */
 	public void putPoint() throws IOException, CourseNotFound, UserNotFound {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter course name: ");
@@ -104,6 +160,14 @@ public class Teacher extends Employee implements Researcher, Serializable {
 		}
 	}
 
+	/**
+	 * Allows the teacher to add a point to a student's journal for a specific course,
+	 * directly using the student and course objects.
+	 *
+	 * @param student the student to add the point for
+	 * @param course the course for which to add the point
+	 * @throws IOException if an error occurs while reading input
+	 */
 	public void putPoint(Student student, Course course) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter point: ");
@@ -112,6 +176,12 @@ public class Teacher extends Employee implements Researcher, Serializable {
 		
 	}
 
+	/**
+	 * Allows the teacher to change a point in a student's journal for a specific course.
+	 *
+	 * @throws IOException if an error occurs while reading input
+	 * @throws ParseException if an error occurs while parsing a date
+	 */
 	public void changePoint() throws IOException, ParseException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter course name: ");
@@ -131,9 +201,23 @@ public class Teacher extends Employee implements Researcher, Serializable {
 		
 	}
 
+	/**
+	 * Allows the teacher to change a point in a student's journal for a specific course,
+	 * directly using the student and course objects.
+	 *
+	 * @param student the student whose point to change
+	 * @param course the course for which to change the point
+	 */
 	public void changePoint(Student student, Course course){
 	}
 
+	/**
+	 * Allows the teacher to record a student's attendance for a specific lesson in a course.
+	 *
+	 * @throws IOException if an error occurs while reading input
+	 * @throws CourseNotFound if the specified course is not found
+	 * @throws UserNotFound if the specified student is not found
+	 */
 	public void putAttendance() throws IOException, CourseNotFound, UserNotFound {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter course name: ");
@@ -183,6 +267,14 @@ public class Teacher extends Employee implements Researcher, Serializable {
 		
 	}
 
+	/**
+	 * Records a student's attendance for a specific lesson in a course.
+	 *
+	 * @param student the student to record attendance for
+	 * @param course the course for which to record attendance
+	 * @throws IOException if an error occurs while reading input
+	 * @throws CourseNotFound if the specified course is not found in the student's journal
+	 */
 	public void putAttendance(Student student, Course course) throws IOException, CourseNotFound {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		if(student.getJournal().getPoints().containsKey(course)){
@@ -220,6 +312,13 @@ public class Teacher extends Employee implements Researcher, Serializable {
 		
 	}
 
+	/**
+	 * Changes a student's attendance record for a specific lesson in a course.
+	 *
+	 * @param student the student whose attendance to change
+	 * @param course the course for which to change attendance
+	 * @throws IOException if an error occurs while reading input
+	 */
 	public void changeAttendance(Student student, Course course) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter a date (format: dd/MM/yyyy): ");
@@ -236,10 +335,20 @@ public class Teacher extends Employee implements Researcher, Serializable {
 		
 	}
 
+	/**
+	 * Gets a map of courses with their associated lists of students.
+	 *
+	 * @return a map of courses and student lists
+	 */
 	public HashMap<Course, List<Student>> getCourseWithStudents() {
 		return courseWithStudents;
 	}
 
+	/**
+	 * Records attestation points for a student in a course.
+	 *
+	 * @throws IOException if an error occurs while reading input
+	 */
 	public void putAttestationPoints() throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter course name: ");
@@ -271,6 +380,11 @@ public class Teacher extends Employee implements Researcher, Serializable {
 		
 	}
 
+	/**
+	 * Records the final exam mark for a student in a course.
+	 *
+	 * @throws IOException if an error occurs while reading input
+	 */
 	public void putFinalExamMark() throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter course name: ");
@@ -290,6 +404,9 @@ public class Teacher extends Employee implements Researcher, Serializable {
 		
 	}
 
+	/**
+	 * Prints a list of courses taught by the teacher.
+	 */
 	public void viewCourses() {
 		int i = 0;
 		for(Course course : courseWithStudents.keySet()){
@@ -298,6 +415,13 @@ public class Teacher extends Employee implements Researcher, Serializable {
 		}
 	}
 
+	/**
+	 * Creates a new order for a student.
+	 *
+	 * @param student the student for whom the order is being created
+	 * @return the new Order object
+	 * @throws IOException if an error occurs while reading input
+	 */
 	private Order createOrder(Student student) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Type description: ");
@@ -318,6 +442,11 @@ public class Teacher extends Employee implements Researcher, Serializable {
 
 	}
 
+	/**
+	 * Sends a complaint about a student.
+	 *
+	 * @throws IOException if an error occurs while reading input
+	 */
 	public void sendComplaint() throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		String name = bf.readLine();
@@ -331,24 +460,42 @@ public class Teacher extends Employee implements Researcher, Serializable {
 		
 	}
 
+	/**
+	 * Sends a request to another employee.
+	 *
+	 * @param to the employee to whom the request is being sent
+	 * @param request the Request object
+	 */
 	public void sendRequest(Employee to, Request request) {
 	}
 
+	/**
+	 * Implements the research-related methods from the Researcher interface.
+	 */
 	@Override
 	public void conductResearch() {
 
 	}
 
+	/**
+	 * Implements the research-related methods from the Researcher interface.
+	 */
 	@Override
 	public int calculateHIndex() {
 		return 0;
 	}
 
+	/**
+	 * Implements the research-related methods from the Researcher interface.
+	 */
 	@Override
 	public void printPapers(Comparator c) {
 
 	}
 
+	/**
+	 * Implements the research-related methods from the Researcher interface.
+	 */
 	@Override
 	public void shareResearchPaper(Researcher to) {
 
