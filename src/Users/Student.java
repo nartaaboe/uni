7f7ac7ec2766d07fba17b3153b788e044fbdfd1c
JohnnyIsNotAvailable <kabevzhanbek@gmail.com>
2023-12-26 22:39:33 +0600

    comment

--- src/Users/Student.java
+++ src/Users/Student.java
@@ -15,17 +15,64 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Represents a student in the system, extending the User class and implementing the Researcher interface.
+ *
+ * <p>Students have a faculty, year of study, enrolled courses, a journal, a transcript, and can perform actions
+ * related to their academic progress.
+ */
 public class Student extends User implements Researcher, Serializable {
+
+
+	/**
+	 * Unique identifier for serialization.
+	 */
 	private static final long serialVersionUID = 1L;
+
+	/**
+	 * The student's faculty.
+	 */
 	private Faculty faculty;
+
+	/**
+	 * The student's journal, containing academic records and notes.
+	 */
 	private Journal journal;
+
+	/**
+	 * A map of courses the student is enrolled in, associated with the teachers for each course.
+	 */
 	private HashMap<Teacher, Course> teacherCourse;
+
+	/**
+	 * The student's current year of study.
+	 */
 	private int yearOfStudy;
+
+	/**
+	 * The student's transcript, containing grades and academic performance.
+	 */
 	private Transcript transcript;
 
+	/**
+	 * Default constructor for creating a Student object.
+	 */
 	public Student(){
 	}
 
+	/**
+	 * Constructor for creating a Student object with detailed information.
+	 *
+	 * @param userType     the student's user type
+	 * @param id           the student's ID
+	 * @param password     the student's password
+	 * @param username     the student's username
+	 * @param faculty      the student's faculty
+	 * @param yearOfStudy  the student's year of study
+	 * @param teacherCourse a map of courses and teachers
+	 * @param journal      the student's journal
+	 * @param transcript   the student's transcript
+	 */
 	public Student(UserType userType, String id, String password, String username, Faculty faculty,
 				   int yearOfStudy, HashMap<Teacher, Course> teacherCourse, Journal journal, Transcript transcript) {
 		super(userType, id, password, username);
@@ -36,14 +83,31 @@ public class Student extends User implements Researcher, Serializable {
 		this.transcript = transcript;
 	}
 
+	/**
+	 * Constructor for creating a Student object with basic information.
+	 *
+	 * @param userType the student's user type
+	 * @param id       the student's ID
+	 * @param password the student's password
+	 * @param username the student's username
+	 */
 	public Student(UserType userType, String id, String password, String username) {
 		super(userType, id, password, username);
 	}
 
+
+	/**
+	 * Gets the student's journal.
+	 *
+	 * @return the student's journal
+	 */
 	public Journal getJournal() {
 		return journal;
 	}
 
+	/**
+	 * Prints a list of courses the student is enrolled in.
+	 */
 	public void viewCourses() {
 		int i = 0;
 		for(Map.Entry<Teacher, Course> entry : teacherCourse.entrySet()){
@@ -52,10 +116,19 @@ public class Student extends User implements Researcher, Serializable {
 		}
 	}
 
+	/**
+	 * Registers the student for a course with a specific teacher.
+	 *
+	 * @param teacher the teacher for the course
+	 * @param course  the course to register for
+	 */
 	public void registerForCourses(Teacher teacher, Course course) {
 
 	}
 
+	/**
+	 * Prints a list of marks for each course, along with the corresponding grades.
+	 */
 	public void viewMarks() {
 		int i = 0;
 		for(Map.Entry<Course, Mark> entry : transcript.getMarks().entrySet()){
@@ -64,63 +137,131 @@ public class Student extends User implements Researcher, Serializable {
 		}
 	}
 
+	/**
+	 * Prints the student's transcript, including marks, grades, and overall GPA.
+	 */
 	public void viewTranscript() {
 		viewMarks();
 		System.out.println("gpa -> " + transcript.getOverallGpa());
 	}
 
+	/**
+	 * Displays the contents of the student's journal.
+	 *
+	 * @throws IOException if an error occurs while accessing the journal
+	 */
 	public void viewJournal() throws IOException {
 		getJournal().showPoints();
 	}
 
+	/**
+	 * Allows the student to rate their teachers.
+	 *
+	 * @implSpec Not yet implemented.
+	 */
 	public void rateTeachers() {
 		// TODO implement me
 	}
 
+	/**
+	 * Returns the student's transcript.
+	 *
+	 * @return the student's transcript
+	 */
 	public Transcript getTranscript() {
 		return transcript;
 	}
 
+	/**
+	 * Gets the student's overall GPA.
+	 *
+	 * @return the student's GPA
+	 */
 	public double getGpa() {
 		return 0.0;
 	}
 
+	/**
+	 * Records the student's attendance for a specific course or activity.
+	 *
+	 * @implSpec Not yet implemented.
+	 */
 	public void putAttendance() {
 		// TODO implement me
 	}
 
+	/**
+	 * Allows the student to conduct research activities.
+	 *
+	 * @implSpec Not yet implemented.
+	 */
 	@Override
 	public void conductResearch() {
 
 	}
 
+	/**
+	 * Calculates the student's h-index, a metric for measuring research impact.
+	 *
+	 * @return the student's h-index
+	 */
 	@Override
 	public int calculateHIndex() {
 		return 0;
 	}
 
+	/**
+	 * Prints a list of the student's research papers sorted using a specific comparator.
+	 *
+	 * @param c the comparator to use for sorting
+	 */
 	@Override
 	public void printPapers(Comparator c) {
 
 	}
 
+	/**
+	 * Shares a research paper with another researcher.
+	 *
+	 * @param to the researcher to share the paper with
+	 */
 	@Override
 	public void shareResearchPaper(Researcher to) {
 
 	}
 
+	/**
+	 * Gets the student's faculty.
+	 *
+	 * @return the student's faculty
+	 */
 	public Faculty getFaculty() {
 		return faculty;
 	}
 
+	/**
+	 * Gets a map of courses and the teachers who teach them for the student.
+	 *
+	 * @return a map of teachers and courses
+	 */
 	public HashMap<Teacher, Course> getTeacherCourse() {
 		return teacherCourse;
 	}
 
+	/**
+	 * Gets the student's current year of study.
+	 *
+	 * @return the student's year of study
+	 */
 	public int getYearOfStudy() {
 		return yearOfStudy;
 	}
 
+	/**
+	 * Returns a string representation of the student, including their name, ID, and faculty.
+	 *
+	 * @return a string representation of the student
+	 */
 	@Override
 	public String toString() {
 		return "name: " + getUsername() + ", ID: " + getId() + ", faculty: " + getFaculty();
--- src/Users/User.java
+++ src/Users/User.java
@@ -11,26 +11,63 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 
-
+/**
+ * An abstract User class representing a user of the university system.
+ */
 public abstract class User implements Serializable {
+
+	/**
+	 * The serial version UID.
+	 */
 	private UserType userType;
+
+	/**
+	 * The user's type (e.g., "student", "teacher", "admin").
+	 */
 	private String id;
+
+	/**
+	 * The user's ID.
+	 */
 	private String password;
+
+	/**
+	 * The user's password.
+	 */
 	private String username;
 
-
+	/**
+	 * The user's username.
+	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * A static set of all users in the system.
+	 */
 	public static HashSet<User> users;
 
+	/**
+	 * A static initializer block that loads user data from the "db/users.ser" file into the {@code users} set.
+	 */
 	static {
 		users = loadUsers();
 	}
 
+	/**
+	 * Creates a new User object with default values.
+	 */
 	public User(){
 
 	}
 
+	/**
+	 * Creates a new User object with the specified user type, ID, password, and username.
+	 *
+	 * @param userType    The user's type (e.g., "student", "teacher", "admin").
+	 * @param id          The user's ID.
+	 * @param password    The user's password.
+	 * @param username    The user's username.
+	 */
 	public User(UserType userType, String id, String password, String username) {
 		this.userType = userType;
 		this.id = id;
@@ -39,26 +76,56 @@ public abstract class User implements Serializable {
 
 	}
 
+	/**
+	 * Returns the user's type.
+	 *
+	 * @return The UserType enum value representing the user's type.
+	 */
 	public UserType getUserType() {
 		return userType;
 	}
 
+	/**
+	 * Returns the user's ID.
+	 *
+	 * @return The user's ID as a String.
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * Returns the user's password.
+	 *
+	 * @return The user's password as a String.
+	 */
 	public String getPassword() {
 		return password;
 	}
 
+	/**
+	 * Returns the user's username.
+	 *
+	 * @return The user's username as a String.
+	 */
 	public String getUsername() {
 		return username;
 	}
 
+	/**
+	 * Returns the user's full name.
+	 *
+	 * @return The user's full name as a String. Implementations in subclasses should provide the appropriate name for each user type.
+	 */
 	public String getName() {
 		return "";
 	}
 
+	/**
+	 * Allows the user to leave a comment on a news item.
+	 *
+	 * @throws IOException If an I/O error occurs while reading user input.
+	 */
 	public void leaveCommentToNews() throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		List<News> news = Data.getInstance().getNews();
@@ -72,26 +139,57 @@ public abstract class User implements Serializable {
 		Data.getInstance().getNews().get(n - 1).leaveComment(new Comment(this, new Date(), comment));
 		
 	}
+
+	/**
+	 * Sets the user's type.
+	 *
+	 * @param userType The new UserType value to set.
+	 */
 	public void setUserType(UserType userType) {
 		this.userType = userType;
 	}
 
+	/**
+	 * Sets the user's ID.
+	 *
+	 * @param id The new ID to set.
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
 
+	/**
+	 * Sets the user's password.
+	 *
+	 * @param password The new password to set.
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+	/**
+	 * Sets the user's username.
+	 *
+	 * @param username The new username to set.
+	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
+	/**
+	 * Changes the user's full name.
+	 *
+	 * @param name The new full name to set.
+	 */
 	public void changeName(String name) {
 	}
 
-
+	/**
+	 * Verifies the user's current password.
+	 *
+	 * @return True if the provided password matches the user's current password, false otherwise.
+	 * @throws IOException If an I/O error occurs while reading user input.
+	 */
 	public boolean verifyPassword() throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter current password: ");
@@ -103,6 +201,11 @@ public abstract class User implements Serializable {
 		return false;
 	}
 
+	/**
+	 * Allows the user to change their password.
+	 *
+	 * @throws IOException If an I/O error occurs while reading user input.
+	 */
 	public void changePassword() throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		verifyPassword();
@@ -122,13 +225,22 @@ public abstract class User implements Serializable {
 		
 	}
 
-
+	/**
+	 * Allows the user to change their phone number.
+	 *
+	 * @param phoneNumber The new phone number to set.
+	 * @return True if the phone number was changed successfully, false otherwise.
+	 */
 	public boolean changePhoneNumber(String phoneNumber) {
 		// TODO implement me
 		return false;
 	}
 
-
+	/**
+	 * A private static method that loads user data from the "db/users.ser" file into a HashSet.
+	 *
+	 * @return A HashSet containing the loaded User objects, or an empty HashSet if the file does not exist or an error occurs.
+	 */
 	private static HashSet<User> loadUsers() {
 		File file = new File("db/users.ser");
 		if (file.exists()) {
@@ -141,6 +253,11 @@ public abstract class User implements Serializable {
 		return new HashSet<>();
 	}
 
+	/**
+	 * A public static method that saves the current set of users to the "db/users.ser" file.
+	 *
+	 * @throws Exception If an error occurs during serialization or file output.
+	 */
 	public static void saveUsers() throws Exception {
 		FileOutputStream fos = new FileOutputStream("db/users.ser");
 		ObjectOutputStream oos = new ObjectOutputStream(fos);
